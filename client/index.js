ad44cd4f16fec7d360b98a42a58bc32fdb403b54
joe <joe@astucc.io>
2017-12-15 13:34:27 -0500

    Setup Webpack loaders configuration

--- client/index.js
+++ client/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/main.css";
 
 // render this node into the DOM
 ReactDOM.render(<div>Hello React!!!</div>, document.getElementById("app"));
--- webpack.config.js
+++ webpack.config.js
@@ -9,13 +9,25 @@ const config = {
   },
   module: {
     // define our loaders here
+    // array of rules to handle different file types
     rules: [
-      // array of rules to handle different file types
       {
         test: /\.(js|jsx)$/, // check for .js and .jsx files (uses Regex)
-        loader: "babel-loader" // use these loaders for .js and .jsx files found
+        loader: "babel-loader" // use this loader for .js and .jsx files found
+      },
+      {
+        // check for files ending with  .css (uses Regex)
+        test: /\.css$/,
+        // use these loaders of .css files. 'css-loader gets run first and is
+        // used to handle the imports of our css files inside our jsx files.
+        // The style loader then mounts our css in to the DOM
+        loaders: ["style-loader", "css-loader"]
       }
     ]
+  },
+  // set the file extensions we want webpack to resolve
+  resolve: {
+    extensions: [".js", ".jsx"]
   }
 };
 
